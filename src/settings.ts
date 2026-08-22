1a179b8a8ce73939508f352c0d706e2e1fb84664
Pauline Swirgiel <pswirgie@1E2.42angouleme.fr>
2026-08-22 12:35:56 +0200

    feat(): base setup with template astro breeze

---
+++ src/settings.ts
@@ -0,0 +1,25 @@
+export const profile = {
+
+};
+
+// Set equal to an empty string to hide the icon that you don't want to display
+export const social = {
+
+};
+
+export const template = {
+  website_url: "https://localhost:4321", // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+  menu_left: false,
+  transitions: true,
+  lightTheme: "light", // Select one of the Daisy UI Themes or create your own
+  darkTheme: "dark", // Select one of the Daisy UI Themes or create your own
+  excerptLength: 200,
+  postPerPage: 5,
+  base: "/PortFolio", // Repository name starting with /
+};
+
+export const seo = {
+  default_title: "Nofy Leclercq | Portfolio",
+  default_description: "Astro Academia is a template for academic websites.",
+  default_image: "/images/astro-academia.png",
+};
